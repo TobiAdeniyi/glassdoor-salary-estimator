f2a5df26a4a3f88145b68c4c071bae0062d6a457
Tobi <AdeniyiSoftware@gmail.com>
2020-07-16 12:42:10 +0100

    uploaded glassdoor scraper

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 16:33:40 2020
+
+@author: tobiadeniyi
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+
+path = "/Users/tobiadeniyi/Documents/Portfolio/Python/ProjectLibrary/glassdoor_proj/chromedriver"
+
+df = gs.get_jobs('data-scientist', 15, False, path, 15)
